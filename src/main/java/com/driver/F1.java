62f1d640bee9da0a821ae5b1cd2f2bd7aea773ac
ajit147481 <rautajit1234@gmail.com>
2022-12-17 18:11:26 +0530

    a

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -1,14 +1,18 @@
 package com.driver;
 
 public class F1 extends Car {
-
+    String name;
+    boolean isManual;
     public F1(String name, boolean isManual) {
         //Use arbitrary values for parameters which are not mentioned
+        this.name=name;
+        this.isManual=isManual;
     }
 
     public void accelerate(int rate){
-        int newSpeed = 0; //set the value of new speed by using currentSpeed and rate
-        /**
+        int newSpeed; //set the value of new speed by using currentSpeed and rate
+        newSpeed=getCurrentSpeed()+rate;
+        /* *
          * speed 0: gear 1
          * speed 1-50: gear 1
          * speed 51-100: gear 2
@@ -18,13 +22,28 @@ public class F1 extends Car {
          * speed more than 250: gear 6
          */
 
+
         if(newSpeed == 0) {
             //Stop the car, set gear as 1
+            stop();
+            changeGear(1);
         }
         //for all other cases, change the gear accordingly
-
-        if(newSpeed > 0) {
-            changeSpeed(newSpeed, getCurrentDirection());
+        else if (newSpeed > 0 && newSpeed<=100) {
+            changeGear(2);
+            move(newSpeed,getCurrentDirection());
+        } else if (newSpeed > 100 && newSpeed<=150) {
+            changeGear(3);
+            move(newSpeed,getCurrentDirection());
+        } else if (newSpeed > 150 && newSpeed<=200) {
+            changeGear(4);
+            move(newSpeed,getCurrentDirection());
+        } else if (newSpeed > 200 && newSpeed<=250) {
+            changeGear(5);
+            move(newSpeed,getCurrentDirection());
+        }else {
+            changeGear(6);
+            move(newSpeed,getCurrentDirection());
         }
     }
 }
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,5 +6,14 @@ public class Main {
         mercedes.move(40, 0);
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
+
+        Car BMW=new Car("BMW",4,4,6,false,"car",4);
+        BMW.changeSpeed(50,0);
+        BMW.changeGear(3);
+
+        Boat b=new Boat("ab",4);
+        b.getVehicleName();
+        b.getVehicleCapacity();
+
     }
 }
